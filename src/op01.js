c85c0c5366198bc1558e640c2c4eb945b5cddff3
juanca-dev <64726022+juanca-dev@users.noreply.github.com>
2021-02-15 11:19:38 -0500

    opcion01

---
+++ src/op01.js
@@ -0,0 +1,14 @@
+// import React, {Component} from 'react';
+
+// function App(){
+//         return(
+//              <div>
+//                 <Header/>
+//                 <SideNav/>
+//                 <MainContent/>     
+//                 <SideNav/>
+//                 <Footer/>
+//             </div>
+//         );
+
+// }
